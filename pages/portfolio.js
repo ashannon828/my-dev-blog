ba2002e6224629ea4ef70f5a1be75de4263ffbc6
ashannon828 <ashannon828@gmail.com>
2020-11-21 14:02:27 +0200

    added portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+import Layout from '../components/layout/Layout';
+
+function portfolio() {
+  return (
+    <Layout>
+      <Heading>Portfolio</Heading>
+    </Layout>
+  );
+}
+
+export default portfolio;
